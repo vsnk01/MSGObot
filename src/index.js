fbdcf3d56892b82282037659d4fc646c0de855db
Fukuro <anya.androschuk@gmail.com>
2024-12-26 23:25:26 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import { bot } from './bot.js';
 import express from 'express';
-// import { startCommand } from './commands/start.command.js';
-// import { helpCommand } from './commands/help.command.js';
 import { registerActions } from './registration/registration.js';
 
 registerActions();
 
 const app = express();
 app.use(express.json());
-app.use(bot.webhookCallback('/secret-path')); // Настройка webhook
-bot.telegram.setWebhook(`https://${process.env.VERCEL_URL}/secret-path`); // Запуск сервера const
+app.use(bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook(`https://${process.env.VERCEL_URL}/secret-path`);
 
 const PORT = 3000;
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
