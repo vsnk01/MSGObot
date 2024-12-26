8668db9e3e37683ff4410261a6540677578b73a7
Fukuro <anya.androschuk@gmail.com>
2024-12-27 00:36:36 +0200

    tesst

--- src/index.js
+++ src/index.js
@@ -5,22 +5,19 @@ import { registerActions } from './registration/registration.js';
 const app = express();
 app.use(express.json());
 app.use(bot.webhookCallback('/secret-path'));
-bot.telegram.setWebhook(`https://${process.env.VERCEL_URL}/secret-path`);
-
-app.get('/', (req, res) => { res.send('Bot is running!')});
-
 const PORT = parseInt(process.env.PORT) || 3000;
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
-
-app.use((err, req, res, next) => {
-  console.error(err.stack); res.status(500).send('Something went wrong!');
-});
 
 registerActions();
 
 bot.launch().then(() => {
+  bot.telegram.setWebhook(`https://${process.env.VERCEL_URL}/secret-path`);
   console.log('Bot launched successfully');
 }).catch((err) => { console.error('Error launching bot:', err); });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack); res.status(500).send('Something went wrong!');
+});
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
