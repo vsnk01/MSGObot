749d1c9bb70ec8ff9f9be6bce5747a1f832d1375
Fukuro <anya.androschuk@gmail.com>
2024-12-27 19:53:51 +0200

    test

--- src/scenes/dialog.scene.js
+++ src/scenes/dialog.scene.js
@@ -43,7 +43,6 @@ dialogScene.on('message', async (context) => {
 
 const enterCustomDialog = async (context) => {
     context.session.adminId = context.from.id;
-    context.session.dialogActive = true;
 
     const customKeyboard = Markup.keyboard([
         [placeholder.endDialogButtonText]
@@ -52,9 +51,10 @@ const enterCustomDialog = async (context) => {
     await context.reply(`Your next messages will be sent directly to @${context.session.user.username}`, customKeyboard);
     await bot.telegram.sendMessage(userId, placeholder.joinChatText('MSGO'));
 
+    context.session.dialogActive = true;
     
     bot.hears(/.*/, async (userContext) => {
-        if (userContext.from.id === userId
+        if (userContext.from.id === context.session.user.userId
             && context.session.dialogActive) {
             await bot.telegram.sendMessage(context.session.adminId, `Message from: @${userContext.from.username}\n\n ${userContext.message.text}`);   
             // if (userContext.message.text === 'End chat') {
