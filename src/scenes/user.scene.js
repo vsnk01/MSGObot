075ca271c1bd471a852acb71a0a487622e8bf081
Fukuro <anya.androschuk@gmail.com>
2024-12-28 01:43:28 +0200

    test

--- src/scenes/user.scene.js
+++ src/scenes/user.scene.js
@@ -1,30 +1,36 @@
-    import { Markup, Scenes } from "telegraf";
-    import { createTimer, proceed } from "../utils/timeout.js";
-    import * as placeholder from "../api/placeholders.js";
+import { Markup, Scenes } from "telegraf";
+import { createTimer, proceed } from "../utils/timeout.js";
+import * as placeholder from "../api/placeholders.js";
     
-    export const userScene = new Scenes.BaseScene('USER_SCENE');
+export const userScene = new Scenes.BaseScene('USER_SCENE');
 
-    userScene.action('apply', proceed(async (context) => {
-        await context.answerCbQuery();
-        await context.scene.enter("APPLICATION_SCENE")
-    }));
-    userScene.action('partnership', proceed(async (context) => {
-        await context.answerCbQuery();
-        await context.reply(placeholder.errorText)
-    }));
-    userScene.action('faq', proceed(async (context) => {
-        await context.answerCbQuery();
-        await context.scene.enter("FAQ_SCENE")
-    }));
+userScene.action('apply', proceed(async (context) => {
+    await context.answerCbQuery();
+    await context.scene.enter("APPLICATION_SCENE")
+}));
+userScene.action('partnership', proceed(async (context) => {
+    await context.answerCbQuery();
+    await context.reply(placeholder.errorText)
+}));
+userScene.action('faq', proceed(async (context) => {
+    await context.answerCbQuery();
+    await context.scene.enter("FAQ_SCENE")
+}));
     
-    userScene.enter(async (context) => {
-        context.session.timeout = createTimer(context);
+userScene.enter(async (context) => {
+    context.session.timeout = createTimer(context);
     
-        const keyboard = Markup.inlineKeyboard([
-            [Markup.button.callback(placeholder.applyButtonText, "apply")],
-            [Markup.button.callback(placeholder.partnershipButtonText, "partnership")],
-            [Markup.button.callback(placeholder.FAQButtonText, "faq")],
-        ]).resize();
+    const keyboard = Markup.inlineKeyboard([
+        [Markup.button.callback(placeholder.applyButtonText, "apply")],
+        [Markup.button.callback(placeholder.partnershipButtonText, "partnership")],
+        [Markup.button.callback(placeholder.FAQButtonText, "faq")],
+    ])
         
-        await context.reply(placeholder.userSceneText(context.from.username), keyboard);
-    });
+    await context.reply(placeholder.userSceneText(context.from.username), keyboard);
+});
+
+userScene.leave((context) => {
+    if (context.session?.timeout) {
+        clearTimeout(context.session.timeout);
+    }
+});
