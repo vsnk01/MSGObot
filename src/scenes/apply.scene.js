6cce50e34bbf4486921990d1c37cd235ec036cfd
Fukuro <anya.androschuk@gmail.com>
2024-12-27 14:11:28 +0200

    test

--- src/scenes/apply.scene.js
+++ src/scenes/apply.scene.js
@@ -1,6 +1,4 @@
 import { Markup, Scenes } from "telegraf";
-import fs from 'fs';
-import path from 'path';
 import { questions } from "../api/application.js";
 import { saveUserData } from "../utils/userlog.js";
 import { createTimer, proceed } from "../utils/timeout.js";
@@ -8,7 +6,7 @@ import { createApplication } from "../utils/createApplication.js";
 import * as placeholder from "../api/placeholders.js";
 
 export const applicationScene = new Scenes.BaseScene("APPLICATION_SCENE");
-const IMAGE_PATH = path.join(__dirname, '../../public/img/title.png');
+const IMAGE_PATH = '../../public/img/title.png';
 
 applicationScene.enter(async (context) => {
   context.session.timeout = createTimer(context);
@@ -16,7 +14,7 @@ applicationScene.enter(async (context) => {
   context.session.answers = [];
 
   await context.replyWithPhoto(
-    { source: fs.createReadStream(IMAGE_PATH) },
+    { photo: IMAGE_PATH },
     { caption: placeholder.joinTeamText }
   );
 
